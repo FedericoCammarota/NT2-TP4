45d6e8f8b28c5a2d3f9be71d2482e613851cb8e0
FedericoCammarota <cammarotafederico@hotmail.com>
2022-05-21 16:22:31 -0300

    Cambio jumbotron

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,17 @@ export default {
 </script>
 
 <style>
+.jumbotron{
+  /* Permalink - use to edit and share this gradient: https://colorzilla.com/gradient-editor/#a7cfdf+0,23538a+100;Blue+3d+%238 */
+background: #a7cfdf; /* Old browsers */
+background: -moz-linear-gradient(-45deg,  #a7cfdf 0%, #23538a 100%); /* FF3.6-15 */
+background: -webkit-linear-gradient(-45deg,  #a7cfdf 0%,#23538a 100%); /* Chrome10-25,Safari5.1-6 */
+background: linear-gradient(135deg,  #a7cfdf 0%,#23538a 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#a7cfdf', endColorstr='#23538a',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+color: white;
+}
+
 h1{
-  color: rgb(12, 11, 11);
+ color: white;
 }
 </style>
